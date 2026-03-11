766ea6297c55cc5d5a78f77c11126e920bcdc8e3
nassro <nassron2024@gmail.com>
2026-03-11 02:15:51 +0100

    Add migrations for sections, utilisateurs, medecins, patients, rendezvous, factures, paiements tables

---
+++ database/migrations/2026_03_11_011022_create_factures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('patients');
+            $table->foreignId('medecin_id')->constrained('medecins');
+            $table->decimal('montant', 8, 2);
+            $table->date('date_facture');
+            $table->string('statut')->default('en attente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factures');
+    }
+};
